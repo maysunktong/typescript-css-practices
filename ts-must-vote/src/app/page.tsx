ac56fb38550049073676dcc2fd15b83ca1d20fe2
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-05-04 00:08:42 +0200

    Done

--- ts-must-vote/src/app/page.tsx
+++ ts-must-vote/src/app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  type Drink = "julmust" | "påsmust";
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  type Votes = {
+    "julmust": number,
+    "påsmust": number
+  }
+
+  const [votes, setVotes] = useState<Votes>({ julmust: 0, påsmust: 0 });
+  const [result, setResult] = useState<string>("Let's vote!");
+
+  const castVote = (drink: Drink) => {
+    return setVotes(prev => ({
+      ...prev, [drink]: prev[drink] + 1
+    }))
+  }
+
+  useEffect(() => {
+    if (votes.julmust === 0 && votes["påsmust"] === 0) {
+      setResult("Let's vote!")
+    } else if (votes.julmust > votes["påsmust"]) {
+
+      setResult("Julmost won!");
+
+    } else if (votes.julmust < votes["påsmust"]) {
+      setResult("Påsmust won!");
+    }
+    else {
+      setResult("Draw.")
+    }
+  }, [votes]);
+
+  const resetVote = () => {
+    setVotes({ julmust: 0, påsmust: 0 })
+  }
+
+  return (
+    <div>
+      <h1>Vote Your Favorite Must 🍾</h1>
+      <div>
+        <h2>Votes:</h2>
+        <span>{votes.julmust}</span> :
+        <span>{votes["påsmust"]}</span>
+      </div>
+      <h1>{result}</h1>
+      <div>
+        <button onClick={() => castVote("julmust")} type="button">Julmust</button>
+        <button onClick={() => castVote("påsmust")} type="button" >Påsmust</button>
+      </div>
+
+      <button onClick={() => resetVote()} type="button">Restart Vote</button>
     </div>
   );
 }
